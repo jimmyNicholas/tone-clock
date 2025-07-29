52162f7ef844d5ea1b510ab6076619b9e3859741
jimmyNicholas <jimmynicholas@duck.com>
2025-07-29 20:44:48 +1000

    style: update font styles and improve text readability across components, including new font families and adjustments to existing text elements

--- src/app/components/AppHeader.tsx
+++ src/app/components/AppHeader.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const AppHeader: React.FC = () => {
   return (
     <header className="text-center mb-4">
-      <h1 className="text-4xl font-bold text-gray-800 mb-2">Tone Clock</h1>
-      <p className="text-gray-600 mb-2">~ Listen to the time ~</p>
+      <h1 className="text-4xl font-bold text-on-surface mb-2">Tone Clock</h1>
+      <p className="text-on-surface/70 mb-2 font-handwriting font-bold text-lg">~ Listen to the time ~</p>
     </header>
   );
 };
